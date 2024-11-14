c8ab9e9dca0470d2c6e070b47037520edbd9009e
ANANYA7788 <ananyahv08@gmail.com>
2024-11-14 09:16:35 +0530

    Update flightRoute.js

--- flightRoute.js
+++ flightRoute.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {createFlight, getAllFlights, updateFlight} from  '../controller/flightController.js';
+import {createFlight, getAllFlights, updateFlight,deleteFlight} from  '../controller/flightController.js';
 
 
 const route=express.Router();
 route.post('/create',createFlight);
 route.get('/getall',getAllFlights)
 route.put('/update/:id',updateFlight);
+route.put('/delete/:id',deleteFlight);
 
 export default route;
